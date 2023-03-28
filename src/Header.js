fcf9f10761857a19fe27d6471bddf495b2ff393e
markusVJH <markus.heinila@pp.inet.fi>
2023-03-28 14:40:00 +0300

    lesson_practice

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+  return <header class="header">
+    <p>Boxes</p>
+    </header>
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Main.css';
+import Box from './Box';
+
+const Main = () => {
+  return (
+   <main>
+  <Box name="Maria" title="CEO" age="29"/>
+  <Box name="Kati" title="Developer" age="25"/>
+  <Box name="Karin" title="Designer" age="45"/>
+  </main>
+);
+};
+
+export default Main;
